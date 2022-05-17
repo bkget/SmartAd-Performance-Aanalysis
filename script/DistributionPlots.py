3a0dc5dc7698f6c9358eb44f32be64c10753e463
Biruk Getaneh <bkgetmom@gmail.com>
2022-05-17 19:43:55 +0000

    Add null and alternative hypothesis plot function

--- script/DistributionPlots.py
+++ script/DistributionPlots.py
@@ -24,7 +24,7 @@ class DistributionPlots:
         ax.bar(x, y, alpha=0.5)
         ax.axvline(x=E_cr * C_total, c='red', alpha=0.75, linestyle='--')
         plt.xlabel('Aware')
-        plt.ylabel('probability')
+        plt.ylabel('Probability')
         plt.show()
         
     def cont_exp_distribution(self, C_aware, E_aware, C_total, E_total, C_cr, E_cr) -> None:
@@ -36,10 +36,27 @@ class DistributionPlots:
         yE = scs.binom(E_total, E_cr).pmf(xE)
         ax.bar(xE, yE, alpha=0.5)
         plt.xlabel('Aware')
-        plt.ylabel('probability')
+        plt.ylabel('Probability')
         #plt.show()
         
     def null_alt_distribution(self, C_total, E_total, C_cr, E_cr) -> None:
         bcr = C_cr
         mde = E_cr - C_cr
         HPP.hypo_plot(C_total, E_total, bcr, mde, show_power=True, show_beta=True, show_alpha=True, show_p_value=True)
+
+
+    def null_alt_distribution1(self, SE_C, SE_E, cont_rate, exp_rate) -> None:
+        fig, ax = plt.subplots(figsize=(12, 6))        
+        x = np.linspace(.35, .6, 1000)
+        yC = scs.norm(cont_rate, SE_C).pdf(x)
+        ax.plot(x, yC, label='Control')
+        ax.axvline(x=cont_rate, c='red', alpha=0.5, linestyle='--')
+
+        yE = scs.norm(exp_rate, SE_E).pdf(x)
+        ax.plot(x, yE, label='Exposed')
+        ax.axvline(x=exp_rate, c='blue', alpha=0.5, linestyle='--')
+
+        plt.legend()
+        plt.xlabel('Awareness Proportion')
+        plt.ylabel('PDF')
+        plt.show()
